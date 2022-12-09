e50fb09f2d3dfa3c1c738c16886d3d56b92e70b4
unknown <basavarajc.xworkz@gmail.com>
2022-12-09 15:48:08 +0530

    Technical programs

---
+++ technical-apti-programs/src/com/javatuts/basicprograms/ReverseString.java
@@ -0,0 +1,13 @@
+/*1. Write a java program to reverse a given String , without using reverse 
+method 
+Input: = “getting good at coding needs a lot of practice” 
+Output: = “practice of lot a needs coding at good getting” */
+package com.javatuts.basicprograms;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+	}
+
+}
